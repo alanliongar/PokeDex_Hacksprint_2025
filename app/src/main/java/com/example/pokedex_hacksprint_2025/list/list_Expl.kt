34c6086f2ec801d03cbcc58f8f09423a92c2ad4d
Alan <alan.lucindo.gomes@alumni.usp.br>
2025-03-04 20:50:01 -0300

    Starting the project

---
+++ app/src/main/java/com/example/pokedex_hacksprint_2025/list/list_Expl.kt
@@ -0,0 +1,8 @@
+package com.example.pokedex_hacksprint_2025.list
+
+class list_Expl {
+    /*
+    * Nesse PACOTE (ou pasta) vamos colocar as informações da função lista: tela de lista, informações que vão pra tela de lista
+    * de onde essas informações vão, o repositório que abastece ela, etc...
+    * */
+}
