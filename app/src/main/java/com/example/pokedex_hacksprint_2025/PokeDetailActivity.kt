7b7c9163a90c7bbc12485484a81c18f0095d183f
brunafenali <brunafenali21@gmail.com>
2025-03-08 09:41:59 -0300

    a imagem do pokemon não esta puxando

--- app/src/main/java/com/example/pokedex_hacksprint_2025/PokeDetailActivity.kt
+++ app/src/main/java/com/example/pokedex_hacksprint_2025/PokeDetailActivity.kt
@@ -12,7 +12,7 @@ class PokeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poke_detail)
 
-        /*// Pegando os componentes da UI
+        // Pegando os componentes da UI
         val pokemonImage = findViewById<ImageView>(R.id.pokemonImage)
         val pokemonName = findViewById<TextView>(R.id.pokemonName)
         val pokemonWeight = findViewById<TextView>(R.id.pokemonWeight)
@@ -22,21 +22,6 @@ class PokeDetailActivity : AppCompatActivity() {
         val attackBar = findViewById<ProgressBar>(R.id.attackBar)
         val defenseBar = findViewById<ProgressBar>(R.id.defenseBar)
 
-        // Recebendo os dados via Intent
-        val name = intent.getStringExtra("POKEMON_NAME") ?: getString(R.string.unknown)
-        val weight = intent.getStringExtra("POKEMON_WEIGHT") ?: "0 KG"
-        val height = intent.getStringExtra("POKEMON_HEIGHT") ?: "0 M"
-        val hp = intent.getIntExtra("POKEMON_HP", 0)
-        val attack = intent.getIntExtra("POKEMON_ATTACK", 0)
-        val defense = intent.getIntExtra("POKEMON_DEFENSE", 0)
 
-        // Atualizando os componentes da UI
-        pokemonName.text = name
-        pokemonWeight.text = getString(R.string.pokemon_weight_label, weight)
-        pokemonHeight.text = getString(R.string.pokemon_height_label, height)
-
-        hpBar.progress = hp
-        attackBar.progress = attack
-        defenseBar.progress = defense*/
     }
 }
