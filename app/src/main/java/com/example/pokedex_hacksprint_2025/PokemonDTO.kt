adeb2db48758b4d2b78e82fd257160c8dc59f159
Alan <alan.lucindo.gomes@alumni.usp.br>
2025-03-07 21:13:38 -0300

    End of the first screen skeleton lol

--- app/src/main/java/com/example/pokedex_hacksprint_2025/PokemonDTO.kt
+++ app/src/main/java/com/example/pokedex_hacksprint_2025/PokemonDTO.kt
@@ -4,8 +4,5 @@ package com.example.pokedex_hacksprint_2025;
 data class PokemonItem(
         val name: String,
         var url: String
-){
-
-}
-
+)
 //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png
